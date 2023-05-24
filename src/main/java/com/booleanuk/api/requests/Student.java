228a4389b13aaf0bfd66fad54ddcc0e6436a038c
Rose <rosevandergiessen@proton.me>
2023-05-24 14:26:44 +0200

    students path REST complete

--- src/main/java/com/booleanuk/api/requests/Student.java
+++ src/main/java/com/booleanuk/api/requests/Student.java
@@ -13,7 +13,15 @@ class Student {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -1,6 +1,7 @@
 package com.booleanuk.api.requests;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -18,12 +19,44 @@ public class Students {
     @ResponseStatus(HttpStatus.CREATED)
     public Student create(@RequestBody Student student) {
         this.students.add(student);
-
         return student;
     }
 
     @GetMapping
     public List<Student> getAll() {
         return this.students;
+    }
+
+    @GetMapping("/{firstName}")
+    public Student getOne(@PathVariable(name = "firstName") String firstName){
+        for (Student s : this.students){
+            if (s.getFirstName().equals(firstName)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping ("/{firstName}")
+    public Student update(@PathVariable(name = "firstName") String firstName, @RequestBody Student student ){
+       for (Student s : this.students) {
+           if (s.getFirstName().equals(firstName)){
+               s.setFirstName(student.getFirstName());
+               s.setLastName(student.getLastName());
+               return s;
+           }
+       }
+       return null;
+    }
+
+    @DeleteMapping("/{firstName}")
+    public Student delete(@PathVariable (name = "firstName") String firstName ) {
+        for (Student s : this.students) {
+            if (s.getFirstName().equals(firstName)) {
+                this.students.remove(s);
+                return s;
+            }
+        }
+        return null;
     }
 }
