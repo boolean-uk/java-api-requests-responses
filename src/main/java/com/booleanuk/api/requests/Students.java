74bcff04ed11a84744349f05104f14b1d795e1e3
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-05-24 14:30:49 +0300

    students API

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -26,4 +26,40 @@ public class Students {
     public List<Student> getAll() {
         return this.students;
     }
+
+    @GetMapping("/{firstname}")
+    public Student getStudent(@PathVariable (name = "firstname") String firstname) {
+        for (Student student : students) {
+            if (student.getFirstName().equals(firstname)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/{firstname}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student updateStudent(@PathVariable (name = "firstname") String firstname, @RequestBody Student student) {
+        for (Student indexStudent : students) {
+            if (indexStudent.getFirstName().equals(firstname)) {
+                int index = this.students.indexOf(indexStudent);
+                this.students.get(index).setFirstName(student.getFirstName());
+                this.students.get(index).setLastName(student.getLastName());
+                return this.students.get(index);
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{firstname}")
+    public Student deleteStudent(@PathVariable (name = "firstname") String firstname) {
+        for (Student student : students) {
+            if (student.getFirstName().equals(firstname)) {
+                int index = this.students.indexOf(student);
+                return this.students.remove(index);
+            }
+        }
+        return null;
+    }
+
 }
