4aaea9487d0549c42a18042eefb0cd0690bad4a5
Irene <irene.maniou@hotmail.com>
2023-09-26 21:15:05 +0300

    Irene Maniou core + extension

---
+++ src/main/java/com/booleanuk/api/Language.java
@@ -0,0 +1,21 @@
+package com.booleanuk.api;
+
+public class Language {
+
+    private String name;
+
+    public Language() {
+    }
+
+    public Language(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
