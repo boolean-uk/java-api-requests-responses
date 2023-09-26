2cdddeaf72cafaa121713c4253f6f86582a3cca3
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-09-26 15:32:29 +0300

    finishes Students-core

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -26,4 +26,45 @@ public class Students {
     public List<Student> getAll() {
         return this.students;
     }
+
+    @GetMapping("/{firstName}")
+    public Student getOne(@PathVariable (name = "firstName") String firstName){
+        for (Student student: students) {
+            if (student.getFirstName().equalsIgnoreCase(firstName)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/{firstName}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student updated(@PathVariable (name = "firstName") String firstName,@RequestBody Student student){
+        int index = -1;
+        for (Student st: students){
+            if (st.getFirstName().equalsIgnoreCase(firstName)){
+                index = this.students.indexOf(st);
+            }
+        }
+        if (index >= 0){
+            this.students.get(index).setFirstName(student.getFirstName());
+            this.students.get(index).setLastName(student.getLastName());
+            return this.students.get(index);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{firstName}")
+    public Student delete(@PathVariable (name = "firstName") String firstName){
+        int index = -1;
+        for (Student student: students){
+            if (student.getFirstName().equalsIgnoreCase(firstName)) {
+                index = this.students.indexOf(student);
+            }
+        }
+        if (index > 0){
+            return this.students.remove(index);
+        }
+        return null;
+    }
 }
