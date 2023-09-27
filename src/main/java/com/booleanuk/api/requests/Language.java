246c40238d0e3c454b6aac4325d8d057e53aa4ae
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-27 04:48:19 +0200

    Added core for API requests using SPEC

--- src/main/java/com/booleanuk/api/requests/Language.java
+++ src/main/java/com/booleanuk/api/requests/Language.java
@@ -2,8 +2,14 @@ package com.booleanuk.api.requests;
 
 public class Language {
     private String name;
-
+    public Language() {
+        this.name = "empty";
+    }
     public Language(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -1,11 +1,80 @@
 package com.booleanuk.api.requests;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
+@RestController
+@RequestMapping("languages")
 public class Languages {
-    private List<Language> languages = new ArrayList<>(){{
+    private List<Language> languages = new ArrayList<>() {{
         add(new Language("Java"));
         add(new Language("C#"));
     }};
+
+
+    @GetMapping
+    public List<Language> getAll() {
+        return this.languages;
+    }
+
+    @GetMapping("/{name}")
+    public Language getOne(@PathVariable(name = "name") String name) {
+        for (Language language : languages) {
+            if (language.getName().equals(name)) {
+                return language;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Language create(@RequestBody Language language) {
+        if (!languages.contains(language) && language.getName() != null) {
+            this.languages.add(language);
+            return language;
+        }
+        return null;
+    }
+
+    @PutMapping("/{name}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Language update(@PathVariable(name = "name") String name, @RequestBody Language language) {
+        for (Language stu : languages) {
+            if (stu.getName().equals(name)) {
+                stu.setName(language.getName());
+                return stu;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{name}")
+    public Language delete(@PathVariable(name = "name") String langName) {
+        //Never delete an item directly using a loop, add it to a var instead
+        //Then delete the added var later on with an if statement
+        Language languageToRemove = null;
+
+        for (Language stu : languages) {
+            if (stu.getName().equals(langName)) {
+                languageToRemove = stu;
+                break;
+            }
+        }
+
+        if (languageToRemove != null) {
+            languages.remove(languageToRemove);
+            return languageToRemove;
+        } else {
+            return null;
+        }
+    }
+
+
+
+
 }
