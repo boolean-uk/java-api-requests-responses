355fb941a2bc1515bfa9b2c2a85595839c7df04b
Nicolai Klokmose <angryniczorn@gmail.com>
2024-01-29 16:05:00 +0100

    Core and Extension

--- src/main/java/com/booleanuk/api/requests/Language.java
+++ src/main/java/com/booleanuk/api/requests/Language.java
@@ -1,13 +1,29 @@
 package com.booleanuk.api.requests;
 
-public class Language {
+class Language {
+    private static int LID = 0;
     private String name;
 
+    private int ID;
+
+    public Language() {
+
+    }
+
     public Language(String name) {
+        this.ID = ++LID;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
