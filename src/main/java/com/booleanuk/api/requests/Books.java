adf36cdf7a4db0bd6f571eabcf0b527093ffccca
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-29 15:13:08 +0100

    Create create method for book

--- src/main/java/com/booleanuk/api/requests/Books.java
+++ src/main/java/com/booleanuk/api/requests/Books.java
@@ -1,5 +1,10 @@
 package com.booleanuk.api.requests;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,5 +17,10 @@ public class Books {
         this.books.add(new Book("BOOK!", 150, "Writer", "Action"));
     }
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book create(@RequestBody Book book) {
+        this.books.add(book);
+        return book;
+    }
 }
