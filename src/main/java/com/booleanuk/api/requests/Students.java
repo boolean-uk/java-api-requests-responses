8ca605c255f226fdaafacc90860e42bbb2d12128
Alexander Niklasson <alexander.niklasson@live.com>
2024-01-29 13:06:40 +0100

    Completed core

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -2,28 +2,70 @@ package com.booleanuk.api.requests;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("students")
 public class Students {
-    private List<Student> students = new ArrayList<>(){{
-        add(new Student("Nathan", "King"));
-        add(new Student("Dave", "Ames"));
-    }};
+    private ArrayList<Student> students;
+
+    public Students(){
+        students = new ArrayList<>();
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Student create(@RequestBody Student student) {
         this.students.add(student);
-
         return student;
     }
 
     @GetMapping
-    public List<Student> getAll() {
+    public ArrayList<Student> getAll() {
         return this.students;
+    }
+
+    @GetMapping("/{name}")
+    public Student getOneStudent(@PathVariable String name){
+        for(Student s : this.students){
+            if(s.getFirstName().equals(name)){
+                return s;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{name}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student updateOneStudent(@PathVariable String name, @RequestBody Student student){
+        if(student.getFirstName() == null && student.getLastName() == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        for(Student s : this.students){
+            if(s.getFirstName().equals(name)){
+                s.setFirstName(student.getFirstName());
+                s.setLastName(student.getLastName());
+                return s;
+            }
+        }
+        throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{name}")
+    public Student deleteOneStudent(@PathVariable String name){
+
+        Iterator<Student> i = this.students.iterator();
+        while(i.hasNext()){
+            Student student = i.next();
+            if(student.getFirstName().equals(name)){
+                i.remove();
+                return student;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
