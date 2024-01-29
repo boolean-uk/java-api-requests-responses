5726139480da80e5910f2b8b60ebf647e6842618
Nikolaos Litsas <litsasnik@outlook.com>
2024-01-29 18:08:56 +0200

    Nikolaos Litsas

--- src/main/java/com/booleanuk/api/requests/Student.java
+++ src/main/java/com/booleanuk/api/requests/Student.java
@@ -4,6 +4,7 @@ class Student {
     private String firstName;
     private String lastName;
 
+
     public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -11,6 +12,14 @@ class Student {
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getLastName() {
