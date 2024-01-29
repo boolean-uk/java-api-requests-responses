69ac1b31294dbc06735928debbd2daa2f21b7bf6
Petter Karstensen <petterkarstensen@gmail.com>
2024-01-29 12:59:00 +0100

    Get single student not working

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -26,4 +26,35 @@ public class Students {
     public List<Student> getAll() {
         return this.students;
     }
+
+    @GetMapping("/{firstname}")
+    public Student getSpecificStudent(@PathVariable String firstName){
+        for (Student s: this.students){
+            if (s.getFirstName().equals(firstName)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/{firstName}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student updateStudent(@PathVariable String firstName, @RequestBody Student student) {
+        for (Student s : this.students) {
+            if (s.getFirstName().equals(firstName)){
+                s.setFirstName(student.getFirstName());
+                s.setLastName(student.getLastName());
+                return s;
+            }
+        }
+        return null;
+    }
+    @DeleteMapping("/{firstName}")
+//    @ResponseStatus(HttpStatus.)
+    public List<Student> deleteStudent(@PathVariable String firstName){
+        this.students.remove(firstName);
+        return this.students;
+    }
+
+
 }
