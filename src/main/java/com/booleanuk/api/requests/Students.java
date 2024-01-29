21727995ba721708eabe39569445163c652efcd3
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-29 13:17:04 +0100

    Create getOne method for student

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -1,6 +1,7 @@
 package com.booleanuk.api.requests;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,10 +10,13 @@ import java.util.List;
 @RestController
 @RequestMapping("students")
 public class Students {
-    private List<Student> students = new ArrayList<>(){{
-        add(new Student("Nathan", "King"));
-        add(new Student("Dave", "Ames"));
-    }};
+    private List<Student> students;
+
+    public Students() {
+        this.students = new ArrayList<>();
+        this.students.add(new Student("Nathan", "King"));
+        this.students.add(new Student("Dave", "Ames"));
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,5 +29,15 @@ public class Students {
     @GetMapping
     public List<Student> getAll() {
         return this.students;
+    }
+
+    @GetMapping("/{firstName}")
+    public ResponseEntity<Student> getOne(@PathVariable(name = "firstName") String firstName) {
+        for (Student student : this.students) {
+            if (student.getFirstName().equals(firstName)) {
+                return ResponseEntity.ok(student);
+            }
+        }
+        return null;
     }
 }
