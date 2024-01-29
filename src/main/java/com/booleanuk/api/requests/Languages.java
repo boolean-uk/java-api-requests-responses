5dc67a748bac9cb9ea2a90e8febb8ba3b6d4c253
Petros Mylonas <petrosmylonascs@gmail.com>
2024-01-29 22:46:03 +0200

    core

--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -1,11 +1,66 @@
 package com.booleanuk.api.requests;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/languages")
 public class Languages {
+
     private List<Language> languages = new ArrayList<>(){{
         add(new Language("Java"));
         add(new Language("C#"));
     }};
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Language createLanguage(@RequestBody Language newLanguage) {
+        languages.add(newLanguage);
+        return newLanguage;
+    }
+
+    @GetMapping
+    public List<Language> getAllLanguages() {
+        return languages;
+    }
+
+    @GetMapping("/{name}")
+    public Language getSpecificLanguage(@PathVariable String name) {
+        return languages.stream()
+                .filter(language -> language.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PutMapping("/{name}")
+    public Language updateLanguage(@PathVariable String name, @RequestBody Language updatedLanguage) {
+        Language languageToUpdate = languages.stream()
+                .filter(language -> language.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+
+        if (languageToUpdate != null) {
+            languageToUpdate.setName(updatedLanguage.getName());
+        }
+
+        return languageToUpdate;
+    }
+
+    @DeleteMapping("/{name}")
+    public Language deleteLanguage(@PathVariable String name) {
+        Language languageToDelete = languages.stream()
+                .filter(language -> language.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+
+        if (languageToDelete != null) {
+            languages.remove(languageToDelete);
+        }
+
+        return languageToDelete;
+    }
 }
+
