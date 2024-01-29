610a67ec81bbb7450559c621c0f4b5b7e1f29f2d
Knut S Rostad <knut.rostad@no.experis.com>
2024-01-29 14:35:04 +0100

    Core done

--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -26,4 +26,40 @@ public class Students {
     public List<Student> getAll() {
         return this.students;
     }
+
+    @GetMapping("/{studentName}")
+    public Student getOne(@PathVariable(name = "studentName") String studentName) {
+        for(Student student : students){
+            if (student.getFirstName().equals(studentName)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/{studentName}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student update(@PathVariable(name = "studentName") String studentName,@RequestBody Student student) {
+        for(Student student1 : students){
+            if (student1.getFirstName().equals(studentName)){
+                student1.setFirstName(student.getFirstName());
+                student1.setLastName(student.getLastName());
+                return student1;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{studentName}")
+    public Student delete(@PathVariable (name = "studentName") String studentName) {
+        for(Student student1 : students){
+            if (student1.getFirstName().equals(studentName)){
+                students.remove(student1);
+                return student1;
+            }
+        }
+        return null;
+    }
+
+
 }
