47fbc451a5285dc5af85476201756cf8965e1169
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-29 13:52:51 +0100

    Create create method for languages

--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -1,11 +1,29 @@
 package com.booleanuk.api.requests;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("languages")
 public class Languages {
-    private List<Language> languages = new ArrayList<>(){{
-        add(new Language("Java"));
-        add(new Language("C#"));
-    }};
+    private List<Language> languages;
+
+    public Languages() {
+        this.languages = new ArrayList<>(){{
+            add(new Language("Java"));
+            add(new Language("C#"));
+        }};
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Language> create(@RequestBody Language language) {
+        this.languages.add(language);
+
+        return ResponseEntity.ok(language);
+    }
 }
