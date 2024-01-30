f88c3de338d20c27ad9451d1f096a0bddf9d5a16
Ateeb <ateeb@live.no>
2024-01-30 02:04:42 +0100

    Ateeb Salam

---
+++ src/main/java/com/booleanuk/api/Book.java
@@ -0,0 +1,57 @@
+package com.booleanuk.api;
+
+public class Book {
+    int id;
+    String title;
+    int numPage;
+    String author;
+    String genre;
+
+    public Book(int id, String title, int numPage, String author, String genre) {
+        this.id = id;
+        this.title = title;
+        this.numPage = numPage;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(int numPage) {
+        this.numPage = numPage;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
