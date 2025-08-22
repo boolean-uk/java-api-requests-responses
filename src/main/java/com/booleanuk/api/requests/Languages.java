1516ea9fdd248e85055b12ae9a0dc0c319d91497
hannaklh <hannaklh@gmail.com>
2025-08-22 14:41:28 +0200

    core exercise

--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -1,11 +1,54 @@
 package com.booleanuk.api.requests;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/languages")
 public class Languages {
     private List<Language> languages = new ArrayList<>(){{
         add(new Language("Java"));
         add(new Language("C#"));
     }};
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Language createLanguage(@RequestBody Language language) {
+        this.languages.add(language);
+        return language;
+    }
+    @GetMapping
+    public List<Language> getLanguages() {
+        return this.languages;
+    }
+    @GetMapping("/{name}")
+    public Language getSpecificLanguage(@PathVariable String name) {
+        Language foundLanguage = languages.stream().filter(l -> l.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+        return foundLanguage;
+    }
+    @PutMapping("/{name}")
+    public Language updateLanguage(@PathVariable String name, @RequestBody Language language) {
+        Language foundLanguage = languages.stream().filter(l -> l.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+        if (foundLanguage != null) {
+            foundLanguage.setName(language.getName());
+
+        }
+        return foundLanguage;
+    }
+    @DeleteMapping("/{name}")
+    public Language deleteLanguage(@PathVariable String name) {
+        Language foundLanguage = languages.stream().filter(language -> language.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+        if (foundLanguage != null) {
+            languages.remove(foundLanguage);
+        }
+        return foundLanguage;
+    }
 }
--- src/main/java/com/booleanuk/api/requests/Students.java
+++ src/main/java/com/booleanuk/api/requests/Students.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping("students")
 public class Students {
-    private List<Student> students = new ArrayList<>(){{
+    private List<Student> students = new ArrayList<>() {{
         add(new Student("Nathan", "King"));
         add(new Student("Dave", "Ames"));
     }};
@@ -23,7 +23,42 @@ public class Students {
     }
 
     @GetMapping
-    public List<Student> getAll() {
+    public List<Student> getStudents() {
+
         return this.students;
+    }
+
+    @GetMapping("/{firstName}")
+    public Student getSpecificStudent(@PathVariable String firstName) {
+
+        Student foundStudent = students.stream()
+                .filter(student -> student.getFirstName().equals(firstName))
+                .findFirst()
+                .orElse(null);
+        return foundStudent;
+    }
+    @PutMapping("/{firstName}")
+    public Student updateStudent(@PathVariable String firstName, @RequestBody Student student) {
+
+        Student foundStudent = students.stream()
+                .filter(s -> s.getFirstName().equals(firstName))
+                .findFirst()
+                .orElse(null);
+        if (foundStudent != null) {
+            foundStudent.setFirstName(student.getFirstName());
+            foundStudent.setLastName(student.getLastName());
+        }
+        return foundStudent;
+    }
+    @DeleteMapping("/{firstName}")
+    public Student deleteStudent(@PathVariable String firstName) {
+        Student foundStudent = students.stream()
+                .filter(s -> s.getFirstName().equals(firstName))
+                .findFirst()
+                .orElse(null);
+        if (foundStudent != null) {
+            students.remove(foundStudent);
+        }
+        return foundStudent;
     }
 }
